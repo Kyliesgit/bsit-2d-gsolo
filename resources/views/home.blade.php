42e86929badca4487e5b52ced9b1081701e7ab50
Kyliesgit <kylekuzma1803@gmail.com>
2023-06-15 13:17:24 -1200

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a href="/products">Products</a>
                 </div>
             </div>
         </div>
